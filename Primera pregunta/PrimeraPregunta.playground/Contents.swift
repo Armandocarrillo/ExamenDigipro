ffe5b5441c4b322eccaccca2bf56b7f51950570c
Armando Carrillo <ingcompuarmando@gmail.com>
2021-01-29 19:14:22 -0600

    Completado

--- Primera pregunta/PrimeraPregunta.playground/Contents.swift
+++ Primera pregunta/PrimeraPregunta.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
 
+enum Genero {
+    case masculino,femenino,persona
+}
+
+struct Humano{
+    var nombre: String
+    var paisOrigen: String
+    var genero: Genero
+}
+
+let humanoPromedio = Humano(nombre: "Armando", paisOrigen: "Mexico", genero: .masculino)
+
+class Profesor {
+
+    var nombre: String = ""
+    var escula: String = ""
+}
+
+class JulioProfe: Profesor {
+    
+}
+
+extension Profesor{
+    
+    func educa() {
+        print("El enseña muy bien")
+    }
+}
+
+
+let profesorEducativo = JulioProfe()
+profesorEducativo.nombre = "Julio"
+profesorEducativo.escula = "Youtube"
+profesorEducativo.educa()
+
